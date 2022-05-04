ec7371a34328a37679df67f29c01be6cdf636a71
felipe <felipe.mk7@gmail.com>
2022-05-04 03:30:17 -0400

    commit 16 - creacion de carpeta, BD y archivos login

---
+++ login/inicio_sesion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Iniciar Sesion</h1>
+<form action="login.php" method="post">
+    <input type="text" name="nombre_login" id="">
+    <input type="password" name="contrasena_login" id="">
+    <button type="submit">Iniciar Sesion</button>
+</form>
+    
+</body>
+</html>
