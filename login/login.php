dbc563d399600d0582b9d6294fd212552fa0d802
felipe <felipe.mk7@gmail.com>
2022-05-14 03:18:09 -0400

    commit 20 - avanzando en login

--- login/login.php
+++ login/login.php
@@ -1,3 +1,43 @@
 <?php
 
 session_start();
+
+include_once "../conexion.php";
+
+$usuario_login = $_POST['nombre_login'];
+$contrasena_login = $_POST['contrasena_login'];
+
+echo '<pre>';
+var_dump($usuario_login);
+var_dump($contrasena_login);
+echo '</pre>';
+
+# VERIFICAR SI EL USUARIO EXISTE
+$sql = 'SELECT * FROM usuarios_crudfetch WHERE nombre = ?';
+$sentencia = $pdo->prepare($sql);
+$sentencia->execute(array($usuario_login));
+$resultado = $sentencia->fetch();
+
+echo '<pre>';
+var_dump($resultado);
+echo '</pre>';
+
+if(!$resultado){
+    echo "usuario no existe";
+    die();
+}
+
+echo '<pre>';
+var_dump($resultado['contrasena']);// recojemos contraseña desde la base de datos
+echo '</pre>';
+
+if(password_verify($contrasena_login, $resultado['contrasena'])){
+    // las contraseñas son iguales
+    $_SESSION['admin']= $usuario_login;
+    header('Location: pagina.html');
+}else{
+    echo 'no son iguales las contraseñas';
+    die();
+}
+
+?>
---
+++ login/registro.php
@@ -0,0 +1,39 @@
+<?php
+include_once "../conexion.php";
+
+# CAPTURAR DATOS POR POST
+$usuario_nuevo = $_POST['nombre_registro'];
+$contrasena = $_POST['contrasena_registro'];
+$contrasena2 = $_POST['contrasena_registro_2'];
+$contrasena_hash = password_hash($contrasena, PASSWORD_DEFAULT); //encriptamos contraseña
+$tipo_usuario = $_POST['tipo_usuario'];
+
+# VERIFICAR SI EL USUARIO EXISTE
+$sql = 'SELECT * FROM usuarios_crudfetch WHERE nombre = ?';
+$sentencia = $pdo->prepare($sql);
+$sentencia->execute(array($usuario_nuevo));
+$resultado = $sentencia->fetch();
+
+# SI EXISTE USUAIRO MATAMOS LA OPERACION
+if($resultado){
+    echo "<br>usuario ya existe";
+    die(); // matamos la operacion. Es como decir exit
+}
+
+# HASH DE CONTRASEÑA
+if(password_verify($contrasena2, $contrasena_hash)){ //funcion php para comparar si la contraseña 1 con la contraseña 2 son iguales. Importante: 1er parametro pass sin encriptar y 2do parametro pass encriptado
+    echo "la contraseña es valida</br>";
+
+    $sql_agregar = 'INSERT INTO usuarios_crudfetch (nombre, contrasena, tipo) values (?,?,?)';
+    $sentencia_agregar = $pdo->prepare($sql_agregar);
+    if($sentencia_agregar->execute(array($usuario_nuevo, $contrasena_hash, $tipo_usuario))){//si esta linea se ejecuta nos devuelve true en caso contrario devuelve false
+        echo "agregado";
+    }
+    
+    $sentencia_agregar = null;
+    $pdo = null;
+}else{
+    echo "la contraseña no es valida";
+}
+
+?>
