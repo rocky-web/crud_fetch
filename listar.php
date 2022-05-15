a2524f2ea203ded75d20a8e7c649d8d0f85bdf79
felipe <felipe.mk7@gmail.com>
2022-05-15 02:27:59 -0400

    commit 24 - backend finalizado

--- listar.php
+++ listar.php
@@ -1,7 +1,7 @@
 <?php
 $data = file_get_contents("php://input");
 require "conexion.php";
-$consulta = $pdo->prepare("SELECT * FROM personas ORDER BY id DESC LIMIT 10 ");
+$consulta = $pdo->prepare("SELECT * FROM personas ORDER BY id DESC LIMIT 50 ");
 $consulta->execute();
 if ($data != "") {
     $consulta = $pdo->prepare("SELECT * FROM personas WHERE rut LIKE '".$data."%' OR nombre LIKE '".$data."%' OR apellidos LIKE '".$data."%' OR concat(nombre,' ',apellidos) LIKE '".$data."%' LIMIT 50 ");
