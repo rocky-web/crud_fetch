9e1b1a240ab8f27a34a178a583cee946bcb72032
Vegeta <felipe.mk7@gmail.com>
2023-03-18 21:44:28 -0300

    css layout changes

--- invitado/inicio_invitado.php
+++ invitado/inicio_invitado.php
@@ -27,49 +27,139 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
     <title>Búsqueda de contratistas</title>
+    <style>
+
+        .fondo{
+            background-color: rgba(160, 167, 167, 0.733);
+        }
+
+        #frm label{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 20px;
+        }
+
+        #frm #buscar{
+            width: 300px;
+            height: 30px;
+        }
+
+        /* link button sesion */
+        .links a{
+            background-color: #4c418f;
+            color: white;
+            padding: 5px 5px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+        }
+
+        .negrita{
+            font-weight: bold;
+            color: blue;
+        }
+    
+        .container_search{
+            /* background-color: yellow; */
+            margin-top: 20px;
+            padding-left: 28px;
+            
+        }
+        .container_title h3{
+            /* background-color: red; */
+            padding: 5px;
+            margin: 0;
+            background-color: blue;
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+            padding-left: 28px;
+            /* text-align: center; */
+            color: white;
+            
+        }
+
+        .container_tabla_inv{
+            /* background-color: aqua; */
+            /* width: 95%; */
+            border: solid blue 2px;
+            overflow-x: scroll;
+        }
+
+        .container_tabla_inv th{
+            background-color: rgb(147, 147, 238);
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+    
+
+        .container_tabla_inv tr:nth-child(odd){
+            background: rgb(177, 177, 171);
+        }
+
+        .container_tabla_inv tr:nth-child(even){
+            background: rgb(159, 167, 161);
+        }
+
+        .container_tabla_inv table{
+            border: solid 2px;
+            width: 100%;
+        }
+
+
+@media only screen and (max-width: 950px) {
+    
+}
+        
+       
+    </style>
 </head>
+
 <body class="fondo">
 
-
-<h3>Búsqueda de contratistas</h3>
-<form id="frm">
-    <label for="">Buscar</label>
-    <input type="text" name="buscar" id="buscar" placeholder="buscar" onkeyup="busqueda()">
-    <button onclick="myAlert()">?</button>
-</form>
-
-<table class="tabla-listar">
-    <thead>
-        <tr>
-            <th>Autorizado</th>
-            <th>Rut</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Empresa</th>
-            <th>Seccion</th>
-            <th>Patente</th>
-            <th>Observaciones</th>
-        </tr>
-    </thead>
+        <div class="container_search">
+            <form id="frm">
+                <label for="">Buscar</label>
+                <input type="text" name="buscar" id="buscar" placeholder="Rut, Nombre, Apellidos, Empresa" onkeyup="busqueda()">
+            </form>
+        </div>
     
-    <tbody id="mostrar">
-       <!--  <?php
-            foreach($resultado as $data){
-                echo "<tr><td>".$data['autorizado']."</td>
-                <td>".$data['rut']."</td>
-                <td>".$data['nombre']."</td>
-                <td>".$data['apellidos']."</td>
-                <td>".$data['empresa']."</td>
-                <td>".$data['seccion']."</td>
-                <td>".$data['patente']."</td>
-                <td>".$data['observaciones']."</td></tr>";
-            }
-        ?> -->
-    </tbody>
-</table>
-
+    
+        <div class="container_title">
+            <h3>Búsqueda de Personas</h3>
+        </div>
+    
+        <div class="container_tabla_inv">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Autorizado</th>
+                        <th>Rut</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Empresa</th>
+                        <th>Seccion</th>
+                        <th>Patente</th>
+                        <th>Observaciones</th>
+                    </tr>
+                </thead>
+                <tbody id="mostrar">
+                   <!--  <?php
+                        foreach($resultado as $data){
+                            echo "<tr><td>".$data['autorizado']."</td>
+                            <td>".$data['rut']."</td>
+                            <td>".$data['nombre']."</td>
+                            <td>".$data['apellidos']."</td>
+                            <td>".$data['empresa']."</td>
+                            <td>".$data['seccion']."</td>
+                            <td>".$data['patente']."</td>
+                            <td>".$data['observaciones']."</td></tr>";
+                        }
+                    ?> -->
+                </tbody>
+            </table>
+        </div>
+    
 
 
 <script>
@@ -94,9 +184,6 @@
 
     busqueda();
 
-    function myAlert(){
-        alert('Puede buscar por: Rut, Nombre, Apellidos o Empresa');
-    }
     
 
 </script>
--- login/inicio_sesion.php
+++ login/inicio_sesion.php
@@ -11,22 +11,76 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+    <style>
+
+        .container{
+            height: 100vh;
+            width: 100%;
+            /* background-color: aqua; */
+            display: grid;
+            align-content: center;
+            justify-content: center;
+            border: solid blue 3px;
+        }
+
+        .container >div:nth-child(1){
+            border: 3px solid red;
+        }
+
+        .container>div> h1{
+           border: 3px solid green;
+        }
+
+        .container_login{
+           
+            border: 3px solid rgb(30, 122, 71);
+            padding: 20px;
+            /* height: 70%;
+            width: 600px; */
+            /* width: 400px; */
+            display: grid;
+            align-content: center;
+            justify-content: center;
+        }
+
+        .container_login> div:nth-child(1){
+            border: 3px solid yellow;
+            padding: 50px;
+            /* width: 500px; */
+            /* background-color: red; */
+            /* padding: 20px; */
+            /* margin: 20px; */
+
+        }
+
+    </style>
     <title>Iniciar Sesion</title>
 </head>
 <body class="fondo">
-
-<h1 class="titulo1"><center>Sistema de Registro de contratistas</center></h1>
-
-<div class="contenedor">
-<form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
-    <h2>Iniciar Sesion</h2>
-    <label for="nom_log">Usuario: </label>
-    <input type="text" name="nombre_login" id="nom_log">
-    <label for="con_log">Contraseña: </label>
-    <input type="password" name="contrasena_login" id="con_log">
-    <button type="submit">Iniciar Sesion</button>
-</form>
+   
+<div class="container">
+    <div>
+        <h1>Iniciar sesión - Sistema de Registro de Personas (copia de github login)</h1>
+        <div class="container_login">
+            <div>
+                <form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
+                    <div>Username</div>
+                    <div><input type="text" name="nombre_login" id="nom_log"></div>
+                    <div>Password</div>
+                    <div><input type="password" name="contrasena_login" id="con_log"></div>
+                    <div><button type="submit">Iniciar Sesion</button></div>
+                </form>
+            </div>
+           <!--  <form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
+                <label for="nom_log">Usuario: </label>
+                <input type="text" name="nombre_login" id="nom_log">
+                <label for="con_log">Contraseña: </label>
+                <input type="password" name="contrasena_login" id="con_log">
+                <button type="submit">Iniciar Sesion</button>
+            </form> -->
+        </div>
+    </div>
 </div>
 
 
