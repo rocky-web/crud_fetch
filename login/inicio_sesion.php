fabfeb10600a8183ffe7b7f8342a1c9c3c15c431
Vegeta <felipe.mk7@gmail.com>
2023-03-18 23:12:30 -0300

    login css improvement

--- login/inicio_sesion.php
+++ login/inicio_sesion.php
@@ -13,47 +13,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="../css/style.css"> -->
     <style>
+        .fondo{
+            margin: 0 auto;
+        }
 
         .container{
-            height: 100vh;
+            /* border: 4px solid orange; */
+            display: grid;
             width: 100%;
-            /* background-color: aqua; */
+            height: 100vh;
+            justify-items: center;
+            align-items: center;
+            box-sizing: border-box;
+        }
+
+        .container h2{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            font-weight: 530;
+            margin: 7px;
+        }
+
+        .container > div{
+            width: fit-content;
+            height: fit-content;
             display: grid;
-            align-content: center;
-            justify-content: center;
-            border: solid blue 3px;
+            justify-items: center;
         }
 
-        .container >div:nth-child(1){
-            border: 3px solid red;
+        form[action="login.php"]{
+            border: 4px solid red;
+            padding: 30px;
+            width: 300px;
+            border-radius: 8px;
+            background-color: gainsboro;
+            border: 1px solid gray;
         }
 
-        .container>div> h1{
-           border: 3px solid green;
+        form[action="login.php"] label, form[action="login.php"] input{
+            display: block;
         }
 
-        .container_login{
-           
-            border: 3px solid rgb(30, 122, 71);
-            padding: 20px;
-            /* height: 70%;
-            width: 600px; */
-            /* width: 400px; */
-            display: grid;
-            align-content: center;
-            justify-content: center;
+        form>:nth-child(1),form>:nth-child(2),form > :nth-child(3),form > :nth-child(4){
+            margin-bottom: 15px;
+        } 
+
+        form input[type="text"], form input[type="password"]{
+            height: 30px;
+            width: 100%;
         }
 
-        .container_login> div:nth-child(1){
-            border: 3px solid yellow;
-            padding: 50px;
-            /* width: 500px; */
-            /* background-color: red; */
-            /* padding: 20px; */
-            /* margin: 20px; */
-
+        form input[type="submit"]{
+            width: 100%;
+            height: 40px;
+            cursor: pointer;
+            background-color: MediumSlateBlue;
+            border: none;
+            border-radius: 5px;
+            color: white;
+            font-size: 16px;
         }
 
+        form input[type="submit"]:hover{
+            background-color: rgb(79, 131, 133);
+            color: white;
+            
+        }
+
+        .container img{
+            width: 100px;
+        }
+        
     </style>
     <title>Iniciar Sesion</title>
 </head>
@@ -61,27 +90,31 @@
    
 <div class="container">
     <div>
-        <h1>Iniciar sesión - Sistema de Registro de Personas (copia de github login)</h1>
-        <div class="container_login">
-            <div>
-                <form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
-                    <div>Username</div>
-                    <div><input type="text" name="nombre_login" id="nom_log"></div>
-                    <div>Password</div>
-                    <div><input type="password" name="contrasena_login" id="con_log"></div>
-                    <div><button type="submit">Iniciar Sesion</button></div>
-                </form>
-            </div>
-           <!--  <form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
-                <label for="nom_log">Usuario: </label>
-                <input type="text" name="nombre_login" id="nom_log">
-                <label for="con_log">Contraseña: </label>
-                <input type="password" name="contrasena_login" id="con_log">
-                <button type="submit">Iniciar Sesion</button>
-            </form> -->
+        <img src="../img/editar.png" alt="">
+        <div>
+            <h2>Sistema de Registro de Personas</h2>
+            <h2>Iniciar sesión</h2>
         </div>
+        <form action="login.php" method="post" autocomplete="off">
+            <label>Username</label>
+            <input type="text" id="nom_log" name="nombre_login">
+            <label>Password</label>
+            <input type="password" id="con_log" name="contrasena_login">
+            <input type="submit" value="Iniciar Sesion">
+        </form>
     </div>
 </div>
+
+
+<!--  
+<form action="login.php" method="post" autocomplete="off" class="form_ini_sesion">
+    <label for="nom_log">Usuario: </label>
+    <input type="text" name="nombre_login" id="nom_log">
+    <label for="con_log">Contraseña: </label>
+    <input type="password" name="contrasena_login" id="con_log">
+    <button type="submit">Iniciar Sesion</button>
+</form> 
+-->
 
 
 </body>
